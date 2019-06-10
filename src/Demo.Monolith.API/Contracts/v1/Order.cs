16e2da4792852b8426cee42de9db68f6297ff87a
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-06-10 17:37:12 +0200

    Include order lines

--- src/Demo.Monolith.API/Contracts/v1/Order.cs
+++ src/Demo.Monolith.API/Contracts/v1/Order.cs
@@ -1,7 +1,11 @@
-﻿namespace Demo.Monolith.API.Contracts.v1
+﻿using System.Collections.Generic;
+
+namespace Demo.Monolith.API.Contracts.v1
 {
     public class Order
     {
         public Customer Customer { get; set; }
+
+        public List<OrderLine> Basket { get; set; } = new List<OrderLine>();
     }
 }
