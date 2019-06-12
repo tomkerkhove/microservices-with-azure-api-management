ebd595380d768fc76dc9f451465a0c114202a901
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-06-12 20:31:05 +0200

    Remove folder per service

--- src/microservices-products/Demo.Microservices.Products.API/Contracts/v1/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demo.Microservices.Products.API.Contracts.v1
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-}
