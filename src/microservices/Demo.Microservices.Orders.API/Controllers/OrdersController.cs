9bc94f1ea12485f4cdec90a3698740aa16213964
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-06-17 09:21:28 +0200

    Provide telemetry
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/microservices/Demo.Microservices.Orders.API/Controllers/OrdersController.cs
+++ src/microservices/Demo.Microservices.Orders.API/Controllers/OrdersController.cs
@@ -5,17 +5,20 @@ using System.Net;
 using System.Threading.Tasks;
 using Demo.Microservices.Orders.API.Managers;
 using Demo.Microservices.Orders.API.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Demo.Microservices.Orders.API.Controllers
 {
     [Route("api/v1/orders")]
     public class OrdersController : Controller
     {
+        private readonly ILogger _logger;
         private readonly OrderManager _orderManager;
         private readonly IOrderRepository _orderRepository;
 
-        public OrdersController(OrderManager orderManager, IOrderRepository orderRepository)
+        public OrdersController(OrderManager orderManager, IOrderRepository orderRepository, ILogger<OrdersController> logger)
         {
+            _logger = logger;
             _orderManager = orderManager;
             _orderRepository = orderRepository;
         }
@@ -32,6 +35,9 @@ namespace Demo.Microservices.Orders.API.Controllers
         public async Task<IActionResult> Post([FromBody]Order orderRequest)
         {
             var orderConfirmation = await _orderManager.CreateAsync(orderRequest);
+
+            _logger.LogInformation("Order {ConfirmationId} was created for {Customer} to deliver at {DeliverAddress} via shipment {TrackingNumber}", orderConfirmation.ConfirmationId, $"{orderConfirmation.Customer.LastName} {orderConfirmation.Customer.FirstName}", orderConfirmation.Customer.Address, orderConfirmation.ShipmentInformation.TrackingNumber);
+
             return CreatedAtAction(nameof(Get), new { ConfirmationId = orderConfirmation.ConfirmationId }, orderConfirmation);
         }
 
@@ -51,8 +57,12 @@ namespace Demo.Microservices.Orders.API.Controllers
             var foundOrder = await _orderRepository.GetAsync(confirmationId);
             if (foundOrder == null)
             {
+                _logger.LogInformation("No information found for order {ConfirmationId} ", confirmationId);
+
                 return NotFound();
             }
+
+            _logger.LogInformation("Information found for order {ConfirmationId} ", confirmationId);
 
             return Ok(foundOrder);
         }
